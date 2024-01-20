55a10cb950b7a731ecc9aeb60d7d3eb5e0038749
socraticprogrammer <thallesgabriel1307@gmail.com>
2024-01-19 21:39:51 -0300

    chore: return interval server error

--- api/src/shared/http/server.ts
+++ api/src/shared/http/server.ts
@@ -8,17 +8,18 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
 
-    next();
-  },
-);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Interval server error',
+  });
+});
 
 app.listen(3333, () => console.info('Server started on 3333 port!'));
